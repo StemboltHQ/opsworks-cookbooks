bd2071e4c8eb34befb2efe70767e44189de024f4
Adam Mueller <adam@freerunningtech.com>
2013-08-02 08:35:23 -0700

    Adds delayed_job/monit configuration cookbook.
    Change-Id: Ic5ced07da32e7ad1834d047290b1fcbe4103f319

---
+++ delayed_job/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: delayed_job
+# Recipe:: default
+#
+# Copyright 2013, FreeRunning Technologies
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template '/etc/monit.d/delayed_job.monitrc' do
+  source 'delayed_job.monitrc.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  variables(
+    :env => node[:deploy][:hats][:rails_env]
+  )
+end
+
+bash 'monit-reload-restart' do
+  user 'root'
+  code 'monit reload && monit'
+end
+
